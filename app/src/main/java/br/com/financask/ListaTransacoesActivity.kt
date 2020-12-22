a4b5a11a6d73bfeda46d7beb650f7c44a9fa8fd9
Raphael Oliveira <oliver.raphaell@gmail.com>
2020-12-21 23:44:17 -0300

    primeiro commit para estudar kotlin alura

---
+++ app/src/main/java/br/com/financask/ListaTransacoesActivity.kt
@@ -0,0 +1,11 @@
+package br.com.financask
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaTransacoesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
