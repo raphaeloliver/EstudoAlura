d8fc8f97dad1e1df340f23c2b16a65070595a5ea
Raphael Oliveira <oliver.raphaell@gmail.com>
2021-01-10 15:32:16 -0300

    cria extensions para implementar sintetic, cria exemplo de listof, arrayAdapter e implementação de um arrayAdapter

--- app/src/main/java/br/com/financask/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/financask/ListaTransacoesActivity.kt
@@ -1,11 +1,19 @@
 package br.com.financask
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 20,50", "Economia - R4 100,00")
+        val arrayAdapter =
+            ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, transacoes)
+
+        lista_transacoes_listview.adapter = arrayAdapter
     }
 }
