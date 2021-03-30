d1fcf729aef7db3550d48baeddd4651420d756c3
raphaelsi <raphaelsi@cielo.com.br>
2021-03-30 00:39:25 -0300

    inclui data, inclui adapter, inclui property

--- app/src/main/java/br/com/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,19 +1,27 @@
-package br.com.financask
+package br.com.financask.ui.activity
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import br.com.financask.R
+import br.com.financask.model.Transacao
+import br.com.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import java.math.BigDecimal
+import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf("Comida - R$ 20,50", "Economia - R4 100,00")
+        val transacoes = listOf(Transacao(BigDecimal(20.5),"comida", Calendar.getInstance()),
+        Transacao(BigDecimal(100.0),"Economia", Calendar.getInstance())
+        )
         val arrayAdapter =
             ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
 
-        lista_transacoes_listview.adapter = arrayAdapter
+        lista_transacoes_listview.adapter =
+            ListaTransacoesAdapter(transacoes, this)
     }
 }
