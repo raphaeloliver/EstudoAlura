8c1729235e228816f87e6707a76c6ab626b3d49a
raphaelsi <raphaelsi@cielo.com.br>
2021-03-30 18:50:25 -0300

    refatoração

--- app/src/main/java/br/com/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,7 +1,6 @@
 package br.com.financask.ui.activity
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import br.com.financask.R
 import br.com.financask.model.Tipo
@@ -15,7 +14,16 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(
+        val transacoes: List<Transacao> = transacoesDeExemplo()
+        configuraLista(transacoes)
+    }
+
+    private fun configuraLista(transacoes: List<Transacao>) {
+        lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
+    }
+
+    private fun transacoesDeExemplo(): List<Transacao> {
+        return listOf(
             Transacao(
                 valor = BigDecimal(20.5),
                 categoria = "Almoço de final de semana",
@@ -36,7 +44,5 @@ class ListaTransacoesActivity : AppCompatActivity() {
                 tipo = Tipo.RECEITA
             )
         )
-        lista_transacoes_listview.adapter =
-            ListaTransacoesAdapter(transacoes, this)
     }
 }
